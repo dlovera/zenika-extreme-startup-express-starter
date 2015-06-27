03096ee3a0ceff4f7a5e5bee657c046172bb1b93
Daniel Lovera <daniel.lovera@zenika.com>
2015-06-27 18:57:38 +0200

    Mise à niveau des différents starters kit

--- extreme_startup_starter.js
+++ extreme_startup_starter.js
@@ -1,7 +1,13 @@
 var express = require("express");
+var server_port = 8080;
 
 var answer = function(question, req, res) {
-    return "<YOUR NAME>";
+    var m = question.match(/what is your name/);
+    if (m) {
+        return "<YOUR NAME>";
+    }
+    console.log("Unrecognized question");
+    return "";
 };
 
 var app = express.createServer();
@@ -12,11 +18,10 @@ app.use(express.session({
 
 app.get("/", function(req, res) {
     var q = req.param("q");
-    var a = answer(q, req, res);
-    console.log("Q: \"" + q + "\" A:\"" + a + "\"");
-    res.end(a);
+    console.log("request: \"" + q + "\"");
+    res.end(answer(q, req, res));
 });
 
-app.listen(8080, "0.0.0.0");
-console.log("Server running on http://0.0.0.0:8080/");
+app.listen(server_port, "0.0.0.0");
+console.log("Server running on http://0.0.0.0:"+ server_port);
 
